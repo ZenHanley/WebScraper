00087ed36d22ebf1973d188e92c6e26933f3ccbe
Zen <zenhanleyreisser@gmail.com>
2025-07-21 18:51:21 +0200

    Changed file name to remove spelling error

---
+++ data.py
@@ -0,0 +1,8 @@
+#-- =========================================== #
+#--                    Data                     #
+#-- =========================================== #
+# Url to Nico's Weg A2 # 
+start="https://learngerman.dw.com/en/user/login"
+email= "zenhanleyreisser@gmail.com"
+password= "Gragknock82"
+A2_url="https://learngerman.dw.com/en/nicos-weg/c-36519797"
--- nico.py
+++ nico.py
@@ -6,33 +6,24 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys 
 import time
+from ..WebScraper import data
 
 #Used to wrap content
 wrap = True
 
 #-- =========================================== #
-#--                    Data                     #
-#-- =========================================== #
-if wrap == True:
-    # Url to Nico's Weg A2
-    start="https://learngerman.dw.com/en/user/login"
-    email= "zenhanleyreisser@gmail.com"
-    password= "Gragknock82"
-    A2_url="https://learngerman.dw.com/en/nicos-weg/c-36519797"
-
-#-- =========================================== #
 #--           Set Service and Driver            #
 #-- =========================================== #
-if wrap == True:
-    # Initialize the Chrome driver with the path to Chromedriver
-    service = Service(executable_path="chromedriver.exe")
-    driver =  webdriver.Chrome(service=service)
-    # Page load with Cloudflare bypass
-    driver.get(start)
-    # Maximize the browser window to ensure all elements are visible to be scraped
-    driver.maximize_window()
-    #Wait for the page to load
-    time.sleep(0.5)
+
+# Initialize the Chrome driver with the path to Chromedriver
+service = Service(executable_path="chromedriver.exe")
+driver =  webdriver.Chrome(service=service)
+# Page load with Cloudflare bypass
+driver.get(start)
+# Maximize the browser window to ensure all elements are visible to be scraped
+driver.maximize_window()
+#Wait for the page to load
+time.sleep(0.5)
 
 #-- =========================================== #
 #--             Entering Information            #
@@ -51,17 +42,11 @@ if wrap == True:
 #-- =========================================== #
 if wrap == True:
     #Clicking login button
-    form = driver.find_element(By.TAG_NAME,"form")
-    login_buttons = form.find_element(By.TAG_NAME,"button")
-    login_buttons.click()
+    driver.find_element(By.CSS_SELECTOR,".sr1l9xy.primary.b17wp39w").click()
     time.sleep(0.5)
     
     #Clicking Confirm Button
-    confirm = driver.find_elements(By.TAG_NAME, "a")
-    for c in confirm:
-        if c.text == "Ok":
-            c.click()
-            break
+    driver.find_element(By.CSS_SELECTOR,".s13pardp.l133n852.sr1l9xy.primary.b17wp39w").click()
     time.sleep(0.5)
 
 #-- =========================================== #
